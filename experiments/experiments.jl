7d01cec1faf7888e86215717cdee38d8f874b0d1
Esteban <e.leivam@uniandes.edu.co>
2024-06-11 19:49:30 -0500

    Aggregated Experiments Ready

--- experiments/experiments.jl
+++ experiments/experiments.jl
@@ -1,6 +1,6 @@
 using PulseAlgorithm
 using Distributions
-
+using PlotlyJS, DataFrames
 include("util.jl")
 
 # Fixed parameters for our experiments
@@ -18,17 +18,18 @@ distance_factor_ChicagoRegional = 0.25 #taken from TransportationsNetworks
 folder_path = raw"data\ChicagoRegional\shortest_paths"
 graph = PulseAlgorithm.load_graph_from_ta(raw"data\ChicagoRegional\ChicagoRegional_net.tntp", 
 raw"data\ChicagoRegional\ChicagoRegional_flow.tntp",  "CR", CV, toll_factor_ChicagoRegional, distance_factor_ChicagoRegional)
+length(graph.nodes)
 covariance_dict = PulseAlgorithm.get_covariance_dict(graph, ρ, max_depth)
 
 sampled_keys = [10584, 9053, 5332, 10172, 6784, 2611, 8851, 11851, 8392, 11012]
-target_node = graph.nodes[sampled_keys[1]].name
 
-n = 10000
-total_instance_info, total_elapsed_time = run_aggregated_experiments(graph, target_node, ρ, α, γ, max_depth, folder_path, network_name, true, n)
+n = 1000
 
-avg_elapsed_time = total_elapsed_time/n
-#avg total instance instance_info
-total_instance_info["pruned_by_bounds"]/n
-total_instance_info["pruned_by_feasibility"]/n
-total_instance_info["total_length_pruned_by_bounds"]/n
-total_instance_info["total_length_pruned_by_feasibility"]/n
+total_instance_info = aggregate_experiment_results(sampled_keys, graph, ρ, α, γ, max_depth, folder_path, network_name, n)
+
+avg_nondominated_paths = total_instance_info["number_nondominanted_paths"] / (length(sampled_keys)*n)
+avg_elapsed_time = total_instance_info["total_elapsed_time"] / (length(sampled_keys)*n)
+avg_pruned_by_bounds = total_instance_info["pruned_by_bounds"] / (length(sampled_keys)*n)
+avg_pruned_by_feasibility = total_instance_info["pruned_by_feasibility"] / (length(sampled_keys)*n)
+avg_length_pruned_by_bounds = (total_instance_info["total_length_pruned_by_bounds"]/total_instance_info["pruned_by_bounds"])
+avg_length_pruned_by_feasibility = (total_instance_info["total_length_pruned_by_feasibility"]/total_instance_info["pruned_by_feasibility"])
--- experiments/util.jl
+++ experiments/util.jl
@@ -3,56 +3,7 @@ using DataStructures
 using CSV
 using Random
 using DataFrames
-
-function modified_dfs(graph::Graph, start_link::Tuple{Int, Int}, max_depth::Int, depth::Int, visited_pairlinks::Dict{Tuple{Int, Int}, Int}, previous_node::Int)
-    if depth > max_depth
-        return visited_pairlinks
-    end
-
-    for adjacent in keys(graph.nodes[start_link[2]].links)
-        if previous_node != adjacent
-            if haskey(visited_pairlinks, (start_link[2], adjacent))
-                visited_pairlinks[(start_link[2], adjacent)] = min(visited_pairlinks[(start_link[2], adjacent)], depth)
-            else
-                visited_pairlinks[(start_link[2], adjacent)] = depth
-            end
-            modified_dfs(graph, (start_link[2], adjacent), max_depth, depth + 1, visited_pairlinks, adjacent)
-        end
-    end       
-
-    return visited_pairlinks
-end
-
-function get_covariance_dict(graph::Graph, ρ::Float64, max_depth::Int)
-    covariance_dict = DefaultDict{Tuple{Int, Int, Int, Int}, Float64}(0.0) #default value of dic is 0.0
-    links = get_links_info(graph)
-    for link in keys(links)
-        visited_pairlinks = modified_dfs(graph, link, max_depth, 1, Dict{Tuple{Int, Int}, Int}(), -1)
-        for pairlink in keys(visited_pairlinks)
-            if pairlink != link 
-                covariance_dict[(link[1], link[2], pairlink[1], pairlink[2])] = (ρ/(visited_pairlinks[pairlink])) * √(links[(pairlink)][3]) * √(links[link][3]) #Corredor structure of ρ's
-            end
-            if pairlink == link
-                covariance_dict[(link[1],link[2],pairlink[1],pairlink[2])] = 1.0
-            end
-        end
-    end
-    return covariance_dict
-end
-
-function get_path_distribution(graph::Graph, path::Vector{Int}, cov_dict::DefaultDict{Tuple{Int, Int, Int, Int}, Float64})
-    mean = 0.0
-    variance = 0.0
-    covariance_term = 0.0
-    for i in 1:length(path)-1
-        mean += graph.nodes[path[i]].links[path[i+1]].mean
-        variance += graph.nodes[path[i]].links[path[i+1]].variance
-        for ii in i + 1:length(path)-1
-            covariance_term += 2*cov_dict[(path[i], path[i+1], path[ii], path[ii+1])]
-        end
-    end
-    return mean, variance, covariance_term
-end
+using ProgressBars
 
 function get_timeBudget(graph::Graph, start_node::Int, target_node::Int, α::Float64, γ::Float64, cov_dict::DefaultDict{Tuple{Int, Int, Int, Int}, Float64})
     shortest_mean_path = dijkstra_between2Nodes(graph, start_node, target_node, "mean")
@@ -62,6 +13,11 @@ function get_timeBudget(graph::Graph, start_node::Int, target_node::Int, α::Flo
     for i in 1:length(shortest_mean_path)-1
         cost_min_mean = cost_min_mean + graph.nodes[shortest_mean_path[i]].links[shortest_mean_path[i+1]].cost 
     end
+
+    cost_min_cost = 0.0
+    for i in 1:length(shortest_cost_path)-1
+        cost_min_cost = cost_min_cost + graph.nodes[shortest_cost_path[i]].links[shortest_cost_path[i+1]].cost 
+    end
     
     mean, variance, covariance_term = get_path_distribution(graph, shortest_mean_path, cov_dict)
     T_t_α = quantile(Normal(mean, √(variance+covariance_term)), α)
@@ -69,8 +25,8 @@ function get_timeBudget(graph::Graph, start_node::Int, target_node::Int, α::Flo
     mean, variance, covariance_term = get_path_distribution(graph, shortest_cost_path, cov_dict)
     T_c_α = quantile(Normal(mean, √(variance+covariance_term)), α)
 
-    T = T_t_α + (T_c_α - T_t_α) * (1 - γ)
-    return T, shortest_mean_path, cost_min_mean
+    T = T_t_α + (T_c_α - T_t_α) * (1 - γ) + 1e-3
+    return T, shortest_mean_path, cost_min_mean, shortest_cost_path, cost_min_cost
 end
 
 function write_shortest_paths(graph::Graph, target_node::String, folder_path::String, network_name::String)
@@ -110,20 +66,22 @@ end
 
 function run_experiments_time(graph::Graph, target_node::String, pulse::SPulseGraph, covariance_dict::DefaultDict, α::Float64, γ::Float64, folder_path::String, network_name::String, initial_bound::Bool)
     source_node = preprocess_experiments(pulse, folder_path, network_name, target_node) 
-    T, shortest_mean_path, cost_min_mean = get_timeBudget(graph, pulse.G.name_to_index[source_node], pulse.G.name_to_index[target_node], α, γ, covariance_dict)
+    T, shortest_mean_path, cost_min_mean, shortest_cost_path, cost_min_cost = get_timeBudget(graph, pulse.G.name_to_index[source_node], pulse.G.name_to_index[target_node], α, γ, covariance_dict)
     pulse.T_max = T
+ 
+    mean_m, variance_m, covariance_term_m = get_path_distribution(graph, shortest_mean_path, covariance_dict)
+    reliability_shortest_mean_path_m = cdf(Normal(mean_m, √(variance_m+covariance_term_m)), T)
 
-    println("Source node: ", source_node)
-    println("Target node: ", target_node)
-    println("Time: ", T)
+    mean_c, variance_c, covariance_term_c = get_path_distribution(graph, shortest_cost_path, covariance_dict)
+    reliability_shortest_cost_path_c = cdf(Normal(mean_c, √(variance_c+covariance_term_c)), T)
 
-    
-    mean, variance, covariance_term = get_path_distribution(graph, shortest_mean_path, covariance_dict)
-    reliability_shortest_mean_path = cdf(Normal(mean, √(variance+covariance_term)), T)
-
-    if reliability_shortest_mean_path >= α && initial_bound
+    if reliability_shortest_mean_path_m >= α && initial_bound
         elapsed_time = @elapsed begin
             run_pulse(pulse, shortest_mean_path, cost_min_mean)
+        end
+    elseif reliability_shortest_cost_path_c >= α && initial_bound
+        elapsed_time = @elapsed begin
+            run_pulse(pulse, shortest_cost_path, cost_min_cost)
         end
     else
         elapsed_time = @elapsed begin
@@ -141,16 +99,41 @@ function run_aggregated_experiments(graph::Graph, target_node::String, ρ::Float
         "pruned_by_bounds" => 0,
         "pruned_by_feasibility" => 0, 
         "total_length_pruned_by_bounds" => 0,
-        "total_length_pruned_by_feasibility" => 0
+        "total_length_pruned_by_feasibility" => 0,
+        "number_nondominanted_paths" => 0, 
+        "total_elapsed_time" => 0.0
     )
-    total_elapsed_time = 0.0
+    
     for i in 1:n
         elapsed_time, instance_info, (start_node, target_node), T, optimal_path = run_experiments_time(graph, target_node, pulse, covariance_dict, α, γ, folder_path, network_name, initial_bound)
         total_instance_info["pruned_by_bounds"] += instance_info["pruned_by_bounds"]
         total_instance_info["pruned_by_feasibility"] += instance_info["pruned_by_feasibility"]
         total_instance_info["total_length_pruned_by_bounds"] += instance_info["total_length_pruned_by_bounds"]
         total_instance_info["total_length_pruned_by_feasibility"] += instance_info["total_length_pruned_by_feasibility"]
-        total_elapsed_time += elapsed_time
+        total_instance_info["number_nondominanted_paths"] += instance_info["number_nondominanted_paths"]
+        total_instance_info["total_elapsed_time"] += elapsed_time
     end
-    return total_instance_info, total_elapsed_time
+    return total_instance_info
+end
+
+function aggregate_experiment_results(sampled_keys::Vector{Int}, graph::Graph, ρ::Float64, α::Float64, γ::Float64, max_depth::Int, folder_path::String, network_name::String, n::Int)
+    total_instance_info = Dict(
+        "pruned_by_bounds" => 0,
+        "pruned_by_feasibility" => 0, 
+        "total_length_pruned_by_bounds" => 0,
+        "total_length_pruned_by_feasibility" => 0,
+        "number_nondominanted_paths" => 0,
+        "total_elapsed_time" => 0.0
+    )
+    for i in ProgressBar(1:length(sampled_keys))
+        key = sampled_keys[i]
+        instance_info = run_aggregated_experiments(graph, graph.nodes[key].name, ρ, α, γ, max_depth, folder_path, network_name, true, n)
+        total_instance_info["pruned_by_bounds"] += instance_info["pruned_by_bounds"]
+        total_instance_info["pruned_by_feasibility"] += instance_info["pruned_by_feasibility"]
+        total_instance_info["total_length_pruned_by_bounds"] += instance_info["total_length_pruned_by_bounds"]
+        total_instance_info["total_length_pruned_by_feasibility"] += instance_info["total_length_pruned_by_feasibility"]
+        total_instance_info["number_nondominanted_paths"] += instance_info["number_nondominanted_paths"]
+        total_instance_info["total_elapsed_time"] += instance_info["total_elapsed_time"]
+    end
+    return total_instance_info
 end
